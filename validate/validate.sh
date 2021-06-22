c8599eaaf904c862aaa0d942b718364ea4465160
Vic Iglesias <viglesias@google.com>
2021-06-22 15:09:40 -0700

    Leverage kpt gatekeeper function

--- validate/validate.sh
+++ validate/validate.sh
@@ -1,8 +1,27 @@
 #!/bin/bash -xe
+
+# Create a temp dir
+CONFIG_DIR=$(mktemp -d)
+
+# deletes the temp directory
+function cleanup {
+  rm -rf "${CONFIG_DIR}"
+}
+# register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+
+# Get Frontend Configs
 cd frontend
-kustomize build k8s/prod > ../prod.yaml
+kustomize build k8s/prod > ${CONFIG_DIR}/frontend.yaml
 cd ..
+
+# Get Backend Configs
 cd backend
-kustomize build k8s/prod >> ../prod.yaml
+kustomize build k8s/prod >> ${CONFIG_DIR}/backend.yaml
 cd ..
-conftest test -p policy ./prod.yaml
+
+# Run them against our constraints
+cp validate/policy.yaml ${CONFIG_DIR}/prod.yaml
+
+# Run the validations
+kpt fn source ${CONFIG_DIR} | kpt fn eval --image gcr.io/kpt-fn/gatekeeper:v0.1 -
