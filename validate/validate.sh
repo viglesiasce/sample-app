28d8fd8e5f8083669d88b676677673d922ed6a78
Vic Iglesias <viglesias@google.com>
2021-06-25 15:04:57 -0700

    Fix validate looping

--- validate/validate.sh
+++ validate/validate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -xe
-
-kpt fn source ../k8s/ | kpt fn eval --image gcr.io/kpt-fn/gatekeeper:v0.1 -
+TMP_DIR=$(mktemp -d)
+kpt fn source -o unwrap ../k8s/ > ${TMP_DIR}/manifests.yaml
+cp ../validate/policy.yaml ${TMP_DIR}/policy.yaml
+kpt fn eval --image $1 --image-pull-policy never ${TMP_DIR}
