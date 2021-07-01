b10361bbf4dabe2bed3cade1e6706f4d646dbaa5
Abby <abigailcarey123@gmail.com>
2021-07-01 13:11:08 -0700

    Removes validation module and switches custom test to run validation image

--- validate/validate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -xe
-TMP_DIR=$(mktemp -d)
-kpt fn source -o unwrap ../k8s/ > ${TMP_DIR}/manifests.yaml
-cp ../validate/policy.yaml ${TMP_DIR}/policy.yaml
-kpt fn eval --image $1 --image-pull-policy never ${TMP_DIR}
